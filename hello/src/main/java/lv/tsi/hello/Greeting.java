819e1daecab71f3fe6305efd79ad04975977c843
Nicolas <nicolasmgc@yahoo.com>
2022-04-18 23:57:19 +0200

    Initial commit

---
+++ hello/src/main/java/lv/tsi/hello/Greeting.java
@@ -0,0 +1,15 @@
+package lv.tsi.hello;
+
+public class Greeting {
+
+    String name;
+
+    public Greeting(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "content: Hello, my name is " + name + "!";
+    }
+}
---
+++ hello/src/main/java/lv/tsi/hello/GreetingController.java
@@ -0,0 +1,14 @@
+package lv.tsi.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+public class GreetingController {
+    @RequestMapping("/greeting")
+        public String greeting(@RequestParam(defaultValue = "Spring") String name) {
+            Greeting greet = new Greeting(name);
+            return greet.toString();
+        }
+
+    }
